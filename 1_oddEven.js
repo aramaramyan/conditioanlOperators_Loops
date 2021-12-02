f1318d569c7165aa0c886d82586e78e29486d864
aramaramyan <aramaramyan1997@mail.ru>
2021-12-02 18:07:19 +0400

    feat: create 1_oddEven.js file and resolve task

---
+++ 1_oddEven.js
@@ -0,0 +1,7 @@
+"use strict";
+
+// Given a number. Print “odd” if the number is odd and “even” if itʼs even.
+
+const num = 125;
+
+(num % 2 === 0) ? console.log(`${num} is an even number.`) : console.log(`${num} is an odd number.`);
