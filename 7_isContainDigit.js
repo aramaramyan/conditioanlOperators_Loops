0e0e5af0f86300f3e0b1b09e1e8178d56a438993
aramaramyan <aramaramyan1997@mail.ru>
2021-12-02 20:10:54 +0400

    feat: create 7_isContainDigit.js file and resolve task

---
+++ 7_isContainDigit.js
@@ -0,0 +1,18 @@
+"use strict";
+
+function isContainDigit(digit, number) {
+
+	const arr = [...(number + "")];
+
+	for (let i = 0; i < arr.length; i++) {
+		if (digit === +arr[i]) {
+			return "Yes"
+		}
+	}
+
+	return "No";
+}
+
+console.log(isContainDigit(5, 2463));  // NO
+console.log(isContainDigit(4, 6));     // NO
+console.log(isContainDigit(8, 45689)); // Yes
