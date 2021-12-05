ee9af6867a522b0bfa9818ee0035f89881260337
aramaramyan <aramaramyan1997@mail.ru>
2021-12-05 12:14:29 +0400

    feat: create 20_printPattern.js file and resolve task

---
+++ 20_printPattern.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/*
+Print the following pattern:
+1
+2 6
+3 7 10
+4 8 11 13
+5 9 12 14 15
+*/
+
+console.log(`1
+2 6
+3 7 10
+4 8 11 13
+5 9 12 14 15`);
+
+// OR
+
+console.log("1\n2 6\n3 7 10\n4 8 11 13\n5 9 12 14 15");
